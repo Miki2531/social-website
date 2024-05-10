b0115917cb865d8e8bc443d79402b67b814607a8
Miki2531 <mikiyasshemelis46@gmail.com>
2024-05-10 14:34:55 +0300

    Implement image_list features on the bookmarked images.

--- images/views.py
+++ images/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import get_object_or_404
 from django.http import JsonResponse
 from django.views.decorators.http import require_POST
 from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+from django.http import HttpResponse
 from django.contrib import messages
 from .forms import ImageCreateForm
 from .models import Image
@@ -52,4 +54,30 @@ def image_like(request):
         except Image.DoesNotExist:
             pass
     return JsonResponse({'status': 'error'})
+
+
+@login_required
+def image_list(request):
+    images = Image.objects.all()
+    paginator = Paginator(images, 8)
+    page = request.GET.get('page')
+    images_only = request.GET.get('images_only')
+
+    try:
+        images = paginator.page(page)
+    except PageNotAnInteger:
+        images = paginator.page(1)
+
+    except EmptyPage:
+        if images_only:
+            return HttpResponse('')
+        images = paginator.page(paginator.num_pages)
+    if images_only:
+        return render(request, 'images/image/list_images.html',
+                       {'section': 'images', 
+                        'images': images})
+    return render(request, 'images/image/list.html', 
+                  {'section': 'images',
+                   'images': images})
+
 
